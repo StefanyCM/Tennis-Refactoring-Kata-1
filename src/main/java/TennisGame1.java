ce22b51126777d98b8a0df7380ac82933e242390
StefanyCM <st.cardenas@udla.edu.co>
2018-10-12 16:41:46 -0500

    Somos Danna Camila Claros Perdomo y Stefany Cárdenas Martínez estudiantes de la Universidad de la Amazonia Caquetá

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,76 +1,109 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_puntaje1 = 0;
-    private int m_puntaje2 = 0;
+    private int marcador_puntaje1 = 0;
+    private int marcador_puntaje2 = 0;
     private String nombreJugador1;
     private String nombreJugador2;
+    String puntajeResultado = "";
 
     public TennisGame1(String nombreJugador1, String nombreJugador2) {
         this.nombreJugador1 = nombreJugador1;
         this.nombreJugador2 = nombreJugador2;
     }
+    
+    
+    
 
     public void ganoPunto(String nombreJugador) {
-        if (nombreJugador == "jugador1")
-            m_puntaje1 += 1;
-        else
-            m_puntaje2 += 1;
+        if (nombreJugador == "jugador1") {
+            marcador_puntaje1 += 1;
+        }else {
+            marcador_puntaje2 += 1;
+        }
     }
 
-    public String getPuntaje() {
-        String puntaje = "";
-        int tempPuntaje=0;
-        if (m_puntaje1 == m_puntaje2)
-        {
-            switch (m_puntaje1)
+    
+    public void verificarEmpate() {
+            switch (marcador_puntaje1)
             {
                 case 0:
-                        puntaje = "Amor-Todos";
+                	puntajeResultado = "Amor-Todos";
                     break;
                 case 1:
-                        puntaje = "Quince-Todos";
+                	puntajeResultado = "Quince-Todos";
                     break;
                 case 2:
-                        puntaje = "Treinta-Todos";
+                	puntajeResultado = "Treinta-Todos";
                     break;
                 default:
-                        puntaje = "Deuce";
+                	puntajeResultado = "Deuce";
                     break;
-                
             }
+    }
+    
+    
+    public void diferenciaPuntajes() {
+    	int diferencia = marcador_puntaje1 - marcador_puntaje2;
+    	
+    	if (diferencia==1) {
+    		puntajeResultado ="Ventaja jugador1";
+    	}
+        else if(diferencia ==-1) {
+        	puntajeResultado ="Ventaja jugador2";
+        }else if (diferencia>=2) {
+        	puntajeResultado = "Gana jugador1";
         }
-        else if (m_puntaje1 >=4 || m_puntaje2 >=4)
-        {
-            int minusResult = m_puntaje1 - m_puntaje2;
-            if (minusResult==1) puntaje ="Ventaja jugador1";
-            else if (minusResult ==-1) puntaje ="Ventaja jugador2";
-            else if (minusResult>=2) puntaje = "Gana jugador1";
-            else puntaje ="Gana jugador2";
+        else {
+        	puntajeResultado ="Gana jugador2";
         }
-        else
+    }
+ 
+    public void puntajeRonda() {
+    	int Puntaje_temporal;
+    	for (int i=1; i<3; i++)
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempPuntaje = m_puntaje1;
-                else { puntaje+="-"; tempPuntaje = m_puntaje2;}
-                switch(tempPuntaje)
-                {
-                    case 0:
-                        puntaje+="Amor";
-                        break;
-                    case 1:
-                        puntaje+="Quince";
-                        break;
-                    case 2:
-                        puntaje+="Treinta";
-                        break;
-                    case 3:
-                        puntaje+="Cuarenta";
-                        break;
-                }
+            if (i==1) {
+            	Puntaje_temporal = marcador_puntaje1;
+            }else {
+            	puntajeResultado+="-"; 
+            	Puntaje_temporal = marcador_puntaje2;
             }
+           puntajeTemporal(Puntaje_temporal);
         }
-        return puntaje;
+    }
+    
+    public void puntajeTemporal (int Puntaje_temporal){
+    	
+    	 switch(Puntaje_temporal)
+         {
+             case 0:
+             	puntajeResultado+="Amor";
+                 break;
+             case 1:
+             	puntajeResultado+="Quince";
+                 break;
+             case 2:
+             	puntajeResultado+="Treinta";
+                 break;
+             case 3:
+             	puntajeResultado+="Cuarenta";
+                 break;
+         }
+    }
+    
+    public String getPuntaje() {
+        
+        if (marcador_puntaje1 == marcador_puntaje2){
+        	verificarEmpate();
+        }       
+        else if (marcador_puntaje1 >=4 || marcador_puntaje2 >=4)
+        {
+            diferenciaPuntajes();            
+        }
+        else {
+            puntajeRonda();
+        }
+        return puntajeResultado;
     }
 }
